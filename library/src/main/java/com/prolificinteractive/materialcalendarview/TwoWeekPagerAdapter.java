f0a95a28c3b9bddbf8c9a1be4e531538cdd1a7b7
Andrei <dan.andrei91@gmail.com>
2018-07-18 15:18:27 -0700

    Two Week View

---
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/TwoWeekPagerAdapter.java
@@ -0,0 +1,89 @@
+package com.prolificinteractive.materialcalendarview;
+
+import android.support.annotation.NonNull;
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+@Experimental
+public class TwoWeekPagerAdapter extends CalendarPagerAdapter<TwoWeekView> {
+
+    public TwoWeekPagerAdapter(MaterialCalendarView mcv) {
+        super(mcv);
+    }
+
+    @Override
+    protected TwoWeekView createView(int position) {
+        return new TwoWeekView(mcv, getItem(position), mcv.getFirstDayOfWeek(), showWeekDays);
+    }
+
+    @Override
+    protected int indexOf(TwoWeekView view) {
+        CalendarDay month = view.getFirstViewDay();
+        return getRangeIndex().indexOf(month);
+    }
+
+    @Override
+    protected boolean isInstanceOfView(Object object) {
+        return object instanceof TwoWeekView;
+    }
+
+    @Override
+    protected DateRangeIndex createRangeIndex(CalendarDay min, CalendarDay max) {
+        return new BiWeekly(min, max, mcv.getFirstDayOfWeek());
+    }
+
+    public static class BiWeekly implements DateRangeIndex {
+
+        private static final int DAYS_IN_TWO_WEEKS = 14;
+        private final CalendarDay min;
+        private final int count;
+
+        public BiWeekly(@NonNull CalendarDay min, @NonNull CalendarDay max, int firstDayOfWeek) {
+            this.min = getFirstDayOfWeek(min, firstDayOfWeek);
+            this.count = weekNumberDifference(this.min, max) + 1;
+        }
+
+        @Override
+        public int getCount() {
+            return count;
+        }
+
+        @Override
+        public int indexOf(CalendarDay day) {
+            return weekNumberDifference(min, day);
+        }
+
+        @Override
+        public CalendarDay getItem(int position) {
+            long minMillis = min.getDate().getTime();
+            long millisOffset = TimeUnit.MILLISECONDS.convert(position * DAYS_IN_TWO_WEEKS, TimeUnit.DAYS);
+            long positionMillis = minMillis + millisOffset;
+            return CalendarDay.from(positionMillis);
+        }
+
+        private int weekNumberDifference(@NonNull CalendarDay min, @NonNull CalendarDay max) {
+            long millisDiff = max.getDate().getTime() - min.getDate().getTime();
+
+            int dstOffsetMax = max.getCalendar().get(Calendar.DST_OFFSET);
+            int dstOffsetMin = min.getCalendar().get(Calendar.DST_OFFSET);
+
+            long dayDiff = TimeUnit.DAYS.convert(millisDiff + dstOffsetMax - dstOffsetMin, TimeUnit.MILLISECONDS);
+            return (int) (dayDiff / DAYS_IN_TWO_WEEKS);
+        }
+
+        /*
+         * Necessary because of how Calendar handles getting the first day of week internally.
+         * /TODO: WTF IS THIS/
+         */
+        private CalendarDay getFirstDayOfWeek(@NonNull CalendarDay min, int wantedFirstDayOfWeek) {
+            Calendar calendar = Calendar.getInstance();
+            min.copyTo(calendar);
+            while (calendar.get(Calendar.DAY_OF_WEEK) != wantedFirstDayOfWeek) {
+                calendar.add(Calendar.DAY_OF_WEEK, -1);
+            }
+            CalendarDay calendarDay = CalendarDay.from(calendar);
+            return CalendarDay.from(calendar);
+        }
+    }
+}
---
+++ library/src/main/java/com/prolificinteractive/materialcalendarview/TwoWeekView.java
@@ -0,0 +1,40 @@
+package com.prolificinteractive.materialcalendarview;
+
+import android.annotation.SuppressLint;
+import android.support.annotation.NonNull;
+
+import java.util.Calendar;
+import java.util.Collection;
+
+@SuppressLint("ViewConstructor")
+public class TwoWeekView extends CalendarPagerView {
+
+    private static int MAX_WEEKS = 2;
+
+    TwoWeekView(@NonNull MaterialCalendarView view, CalendarDay month, int firstDayOfWeek, boolean showWeekDays) {
+        super(view, month, firstDayOfWeek, showWeekDays);
+    }
+
+    @Override
+    protected void buildDayViews(Collection<DayView> dayViews, Calendar calendar) {
+        for (int w = 0; w < MAX_WEEKS; w++) {
+            for (int d = 0; d < DEFAULT_DAYS_IN_WEEK; d++) {
+                addDayView(dayViews, calendar);
+            }
+        }
+    }
+
+    public CalendarDay getMonth() {
+        return getFirstViewDay();
+    }
+
+    @Override
+    protected boolean isDayEnabled(CalendarDay day) {
+        return true;
+    }
+
+    @Override
+    protected int getRows() {
+        return showWeekDays ? MAX_WEEKS + DAY_NAMES_ROW : MAX_WEEKS;
+    }
+}
